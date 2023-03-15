fba6862bcac4fc01f04938997ef796925ff430e5
lakihaomal <lhw150030@utdallas.edu>
2023-03-15 10:43:08 -0500

    python syncher

--- firmware/c_1_pythonSyncher.py
+++ firmware/c_1_pythonSyncher.py
@@ -9,24 +9,11 @@ from mintsXU4 import mintsDefinitions as mD
 
 nodeIDs              = mD.mintsDefinitions['nodeIDs']
 airMarID             = mD.mintsDefinitions['airmarID']
-
+dataFolder           = mD.dataFolder  + "/ref/"
 
 print()
 print("MINTS")
 print()
-
-yamlFile =  str(sys.argv[1])
-print("YAML File: " + yamlFile)
-print()
-
-with open(yamlFile) as file:
-    # The FullLoader parameter handles the conversion from YAML
-    # scalar values to Python the dictionary format
-    mintsDefinitions = yaml.load(file, Loader=yaml.FullLoader)
-
-# airMarID = mintsDefinitions['airmarID']
-
-dataFolder = mD.dataFolder  + "/ref/"
 
 sysStr = 'rsync -avzrtu -e "ssh" ' +  "--include='*.csv' --include='*/' --exclude='*' /home/mints/Downloads/reference/" + airMarID + " " + dataFolder
 print(sysStr)
