3103ccb9351afe7ab8ddb1999ebf9ce2af2784eb
lakihaomal <lhw150030@utdallas.edu>
2023-03-15 11:55:47 -0500

    dataPrepUpdates

--- firmware/mintsXU4/mintsProcessing.py
+++ firmware/mintsXU4/mintsProcessing.py
@@ -190,7 +190,7 @@ def sensorReaderV2(nodeID,sensorID,floatSum1,floatSum2):
 
                 dataNow  = dataNow[sensorDefinitions(sensorID)]
                 dataNow =dataNow.set_index('dateTime').resample(timeSpan).mean()
-                print(dataNow)
+
                 dataIn.append(dataNow)            
         
             if(floatSum2 == floatSumNow):
@@ -199,7 +199,7 @@ def sensorReaderV2(nodeID,sensorID,floatSum1,floatSum2):
 
                 dataNow  = dataNow[sensorDefinitions(sensorID)]
                 dataNow =dataNow.set_index('dateTime').resample(timeSpan).mean()
-                # print(dataNow)
+                
                 dataIn.append(dataNow)             
 
         except Exception as e:
